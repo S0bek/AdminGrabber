ff83475a734ed7e2fde34a2d3bd03b7d08ddc54e
S0bek <dav_fed@outlook.com>
2015-07-18 22:17:13 +0200

    Debut de resultat des requêtes, prochaine étape: extraction de la réponse serveur et ajout des recherches de page php/asp/jsp...

--- UrlParser.pl
+++ UrlParser.pl
@@ -3,12 +3,17 @@
 #Usage: ./UrlParser.pl -s http://www.google.com -f results.txt
 use strict;
 use Getopt::Std;#module utilisé pour la gestion des arguments
-#http request use
+use LWP::UserAgent;#requêtes http
+use MIME::Base64;#envoi des données user et password sous forme encodée pour http
+#use Crypt::SSLeay;#permettre la gestion de http over ssl
 
 #gestion des paramètres
 my %opts = ();#hash qui contiendra les paramètres du programme
 getopt("s:f:" , \%opts);
 my $opt = 0;
+
+#compte factice pour tenter l'authentification sur le site web donné, la connexion peut aussi être un succès...
+my $credentials = "admin:password";
 
 #utilisation du programme
 sub usage {
@@ -42,10 +47,83 @@ sub log_results {
 }
 
 sub try_admin {
-  
+
+  my ($url , $base) = ($opts{s} , "");
+  my $alt_port = 8080;
+
+  if ($url =~ m/\/\//) {
+
+    #suppression du dernier "/" si celui-ci est présent dans l'url donnée
+    if($url =~ /\/$/) {
+      chop($url);
+    }
+
+
+    my @admin_url = (
+                  "/admin",
+                  "/administration" ,
+                  "/admin_panel" ,
+                  "/adminpanel" ,
+                  "/administrator",
+                  "/admin_url",
+                  "/admin-login",
+                  "/admin-url",
+                  "/admin-panel",
+                  "/administration_panel",
+                  "/administration-panel",
+                  "/adm",
+                  "/adm_url",
+                  "/admin_login",
+                  "/login",
+                  "/login_panel",
+                  "/login-panel",
+                  "/administrator_panel",
+                  "/auth",
+                  "/auth-login",
+                  "/auth_login",
+                  "/authentication",
+                  #wordpress url
+                  "/wp-admin",
+                  "/wp-login"
+                  );
+
+    foreach (@admin_url) {
+      my $try = "$url$_";
+      my $alt = "$url:$alt_port$_";
+
+      #on initialise la requête
+      #preparation de la requête avec le UserAgent
+      my $ua = LWP::UserAgent->new();
+
+      my $req = HTTP::Request->new( GET => $try );
+      my $alt_req = HTTP::Request->new( GET => $alt );
+
+      #gestion des token d'authentification
+      my $token = encode_base64($credentials);
+      $req->header( Autorization => "Basic $token" );
+      $alt_req->header( Autorization => "Basic $token" );
+
+      #on demarre la requête
+      my $res = $ua->request($req);
+      print "$res\n";
+      #my $alt_res = $ua->request($alt_req);
+      #print $alt_res."\n";
+
+      print "[CHECK] Tentative avec l'url $try\n";
+      print "[CHECK] Tentative avec l'url $alt\n";
+    }
+
+  } else {
+    my $usage = usage();
+    die "$usage";
+  }
+
+  #my @admin_url = ("http://");
 }
 
 if ($opt == 2) {
-
+  try_admin();
   #log_results("tested\n");
+} else {
+  try_admin();
 }
